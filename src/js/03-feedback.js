c36a613da3b7d6ec8498e3232f05338244c3618b
OksanaKuzich <komoks255@gmail.com>
2022-09-04 11:19:17 +0300

    task 3 don't work script

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
 
+console.log(refs.textarea);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+populateTextarea();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onTextareaInput(event) {
+  const message = event.currentTarget.value;
+  console.log(message);
+  localStorage.setItem('feedback-form-state', message);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
