f77d6023303dd1c246395d9405710659bcd39202
OksanaKuzich <komoks255@gmail.com>
2022-09-04 22:21:04 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,49 @@
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+import throttle from 'lodash.throttle';
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-// const onPlay = function (data) {
-// };
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 
-// player.on('play', onPlay);
+const onPlay = function (data) {
+  const time = data.seconds;
+  localStorage.setItem('videoplayer-current-time', time);
+};
 
-// localStorage.setItem('videoplayer-current-time', 'timeupdate');
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+const obj = {
+  name: 'Max',
+  age: 21,
+  getContact() {
+    console.log('Name: ', this.name, 'Age: ', this.age);
+  },
+  pets: {
+    cat: { name: 'Will' },
+  },
+};
+
+obj.getContact();
